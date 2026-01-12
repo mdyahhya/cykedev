c59a2e2969f2a3959b22fb011ef545b2ee770d2e
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2026-01-12 08:59:04 +0530

    Update BikeViewer3D.tsx

--- src/BikeViewer3D.tsx
+++ src/BikeViewer3D.tsx
@@ -24,7 +24,7 @@ const BikeViewer3D: React.FC<BikeViewer3DProps> = ({ modelPath, isMobile = false
 
     // Camera
  const camera = new THREE.PerspectiveCamera(
-  isMobile ? 55 : 70,
+  isMobile ? 65 : 70,
   mountRef.current.clientWidth / mountRef.current.clientHeight,
   0.1,
   1000
