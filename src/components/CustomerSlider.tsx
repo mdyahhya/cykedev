c9c5b4fff3aec3fb784a3a3cb595168ac763796a
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2026-01-12 07:06:02 +0530

    Refactor CustomerSlider component for clarity

--- src/components/CustomerSlider.tsx
+++ src/components/CustomerSlider.tsx
@@ -109,7 +109,7 @@ const CustomerSlider: React.FC = () => {
               drag="x"
               dragConstraints={{ left: 0, right: 0 }}
               dragElastic={1}
-              onDragEnd={(e, { offset, velocity }) => {
+              onDragEnd={(_e, { offset, velocity }) => {
                 const swipe = swipePower(offset.x, velocity.x);
 
                 if (swipe < -swipeConfidenceThreshold) {
